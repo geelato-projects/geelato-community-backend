b8f9d12778086ddcb3baec848a9cd542c8c7cd47
geelato <geelato@126.com>
2024-08-18 09:50:42 +0800

    更新描述

--- geelato-lang/src/main/java/cn/geelato/lang/api/ApiPagedResult.java
+++ geelato-lang/src/main/java/cn/geelato/lang/api/ApiPagedResult.java
@@ -13,14 +13,29 @@ import lombok.Setter;
 @Setter
 public class ApiPagedResult<E> extends ApiResult<E> {
 
+    /**
+     * 符合查询条件且不分页时的总记录数（相当于dataTotal）
+     */
     private long total;
 
+    /**
+     *  第几页，从1开始
+     */
     private long page;
 
+    /**
+     *  每页最多可以展示的记录数，相当于pageSize（当前实体是PagedResult，这个相当于是省列了page字样，留size）
+     */
     private int size;
 
+    /**
+     * 元数据信息，一般用于实体查询，对查询结果字段的定义信息
+     */
     private Object meta;
 
+    /**
+     * 本次分页查询的实得记录数，相当于pageDataSize（当前实体是PagedResult，这个相当于是省列了page字样，留dataSize）
+     */
     private int dataSize;
 
     public static <T> ApiPagedResult<T> success(T data,long page,int size,int dataSize,long total) {

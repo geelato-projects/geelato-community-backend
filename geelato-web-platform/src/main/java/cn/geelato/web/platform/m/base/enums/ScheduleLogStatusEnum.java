b1f965e660da55acd0f3c184bc9b427d6ec583c6
diablo1303 <diabloposeidon@hotmail.com>
2024-09-10 14:16:22 +0800

    调度执行状态

--- geelato-web-platform/src/main/java/cn/geelato/web/platform/m/base/enums/ScheduleLogStatusEnum.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/m/base/enums/ScheduleLogStatusEnum.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ScheduleLogStatusEnum {
     FAIL("失败", 0),
-    SUCCESS("成功", 1);
+    SUCCESS("成功", 1),
+    EXECUTING("执行中", 2);
 
     private final String label;// 选项内容
     private final int value;// 选项值

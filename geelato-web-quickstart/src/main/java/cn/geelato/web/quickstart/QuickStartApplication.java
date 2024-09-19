7022405e83ce782901a213727d51d73ca483e2d0
gechenvegstall <chengx@bingosoft.net>
2024-09-19 10:02:25 +0800

    schedule新增单独线程池

--- geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/QuickStartApplication.java
+++ geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/QuickStartApplication.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @ComponentScan(basePackages = {"cn.geelato"})
 @EnableCaching
 @Slf4j

6991f913788f25a5aab58d970530649a46c67721
gechenvegstall <maketheidsohard@gmail.com>
2024-11-28 11:48:57 +0800

    完善pdf ocr

--- geelato-web-platform/src/main/java/cn/geelato/web/platform/m/ocr/OCRController.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/m/ocr/OCRController.java
@@ -2,10 +2,7 @@ package cn.geelato.web.platform.m.ocr;
 
 import cn.geelato.lang.api.ApiResult;
 import cn.geelato.plugin.PluginBeanProvider;
-import cn.geelato.plugin.ocr.OCRService;
-import cn.geelato.plugin.ocr.PDFAnnotationMeta;
-import cn.geelato.plugin.ocr.PDFResolveData;
-import cn.geelato.plugin.ocr.PluginInfo;
+import cn.geelato.plugin.ocr.*;
 import cn.geelato.utils.FileUtils;
 import cn.geelato.web.platform.annotation.ApiRestController;
 import cn.geelato.web.platform.m.BaseController;
@@ -42,13 +39,18 @@ public class OCRController extends BaseController {
     }
 
     @RequestMapping(value = "/pdf/resolve", method = RequestMethod.GET)
-    public ApiResult<PDFResolveData> meta(String fileId, String templateId){
+    public ApiResult<?> meta(String fileId, String templateId,boolean wholeContent){
         Attach file = attachService.getModel(fileId);
         Attach template = attachService.getModel(templateId);
         File pdfFile = FileUtils.pathToFile(file.getPath());
         File templateFile = FileUtils.pathToFile(template.getPath());
         OCRService ocrService= pluginBeanProvider.getBean(OCRService.class, PluginInfo.PluginId);
-        PDFResolveData pdfResolveData= ocrService.resolvePDFFile(templateFile,pdfFile);
-        return ApiResult.success(pdfResolveData);
+        if(wholeContent){
+            PDFResolveData pdfResolveData= ocrService.resolvePDFFile(templateFile,pdfFile);
+            return ApiResult.success(pdfResolveData);
+        }else{
+            List<PDFAnnotationPickContent> pdfAnnotationPickContentList= ocrService.pickPDFAnnotationContent(templateFile,pdfFile);
+            return ApiResult.success(pdfAnnotationPickContentList);
+        }
     }
 }

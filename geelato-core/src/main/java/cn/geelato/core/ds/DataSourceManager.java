576bc020d246266bb48c61420ee5716f1b5a011b
gechenvegstall <maketheidsohard@gmail.com>
2025-02-12 16:02:01 +0800

    引入sqlserver驱动

--- geelato-core/src/main/java/cn/geelato/core/ds/DataSourceManager.java
+++ geelato-core/src/main/java/cn/geelato/core/ds/DataSourceManager.java
@@ -63,21 +63,34 @@ public class DataSourceManager extends AbstractManager {
     }
     public DataSource buildDataSource(Map dbConnectMap){
         HikariConfig config = new HikariConfig();
+        String dbType=dbConnectMap.get("db_type").toString().toLowerCase();
         String serverHost=dbConnectMap.get("db_hostname_ip").toString();
         String serverPort=dbConnectMap.get("db_port").toString();
-        String dbName=dbConnectMap.get("db_name").toString();;
-        String commonParams="useUnicode=true&characterEncoding=utf-8&useSSL=false&allowMultiQueries=true&serverTimezone=GMT%2B8&allowPublicKeyRetrieval=true";
-        String jdbcUrl=String.format("jdbc:mysql://%s:%s/%s?%s",serverHost,serverPort,dbName,commonParams);
         String dbUserName=dbConnectMap.get("db_user_name").toString();
         String dbPassWord=dbConnectMap.get("db_password").toString();
-        String dbDriver="com.mysql.cj.jdbc.Driver";
+        String dbName=dbConnectMap.get("db_name").toString();
+        String dbDriver=null;
+        String jdbcUrl=null;
+        switch (dbType){
+            case "mysql":
+                dbDriver="com.mysql.cj.jdbc.Driver";
+                String commonParams="useUnicode=true&characterEncoding=utf-8&useSSL=false&allowMultiQueries=true&serverTimezone=GMT%2B8&allowPublicKeyRetrieval=true";
+                jdbcUrl=String.format("jdbc:mysql://%s:%s/%s?%s",serverHost,serverPort,dbName,commonParams);
+                break;
+            case "sqlserver":
+                dbDriver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+                jdbcUrl="jdbc:sqlserver://"+serverHost+":"+serverPort+";databaseName="+dbName;
+                break;
+            default:
+                break;
+        }
         config.setJdbcUrl(jdbcUrl);
         config.setUsername(dbUserName);
         config.setPassword(dbPassWord);
         config.setDriverClassName(dbDriver);
         config.setMinimumIdle(1);
         config.setMaximumPoolSize(3);
-        config.setPoolName(dbConnectMap.get("db_name").toString());
+        config.setPoolName(dbName);
         config.addDataSourceProperty("autoReconnect","true");
         config.addDataSourceProperty("maxReconnects","10");
         return new HikariDataSource(config);

83bedf786379fac56bd4271a619bf68534a0a3c7
diablo1303 <diabloposeidon@hotmail.com>
2025-03-03 18:37:46 +0800

    优化 HttpUtils 类并添加文档注释
    - 在 doGet 和 doPost 方法中增加对 headers 的非空检查，提高代码健壮性- 添加方法的文档注释，说明参数和返回值的含义，提高代码可读性
    -优化代码格式，使结构更加清晰

--- geelato-utils/src/main/java/cn/geelato/utils/HttpUtils.java
+++ geelato-utils/src/main/java/cn/geelato/utils/HttpUtils.java
@@ -8,12 +8,21 @@ import java.util.Map;
 public class HttpUtils {
     private static final OkHttpClient client = new OkHttpClient();
 
+    /**
+     * 通过HTTP GET请求获取指定URL的内容。
+     *
+     * @param url     请求的URL地址
+     * @param headers 可选的HTTP请求头信息，以Map形式传递，键为头名称，值为头值
+     * @return 返回从URL获取的内容，以字符串形式表示
+     * @throws IOException 如果在请求过程中出现I/O错误，或者服务器返回非成功的HTTP状态码，则抛出此异常
+     */
     public static String doGet(String url, Map<String, String> headers) throws IOException {
-        Request.Builder requestBuilder = new Request.Builder()
-                .url(url);
+        Request.Builder requestBuilder = new Request.Builder().url(url);
         if (headers != null) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
-                requestBuilder.addHeader(entry.getKey(), entry.getValue());
+                if (entry.getKey() != null && entry.getValue() != null) {
+                    requestBuilder.addHeader(entry.getKey(), entry.getValue());
+                }
             }
         }
         Request request = requestBuilder.build();
@@ -26,16 +35,24 @@ public class HttpUtils {
         }
     }
 
+    /**
+     * 通过HTTP POST请求向指定URL发送JSON数据，并返回服务器的响应内容。
+     *
+     * @param url     请求的URL地址
+     * @param json    要发送的JSON数据
+     * @param headers 可选的HTTP请求头信息，以Map形式传递，键为头名称，值为头值
+     * @return 返回从服务器获取的内容，以字符串形式表示
+     * @throws IOException 如果在请求过程中出现I/O错误，或者服务器返回非成功的HTTP状态码，则抛出此异常
+     */
     public static String doPost(String url, String json, Map<String, String> headers) throws IOException {
         MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
         RequestBody body = RequestBody.create(mediaType, json);
-        Request.Builder requestBuilder = new Request.Builder()
-                .url(url)
-                .post(body);
-
+        Request.Builder requestBuilder = new Request.Builder().url(url).post(body);
         if (headers != null) {
             for (Map.Entry<String, String> entry : headers.entrySet()) {
-                requestBuilder.addHeader(entry.getKey(), entry.getValue());
+                if (entry.getKey() != null && entry.getValue() != null) {
+                    requestBuilder.addHeader(entry.getKey(), entry.getValue());
+                }
             }
         }
         Request request = requestBuilder.build();

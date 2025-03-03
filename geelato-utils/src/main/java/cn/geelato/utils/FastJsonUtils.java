a6f91a825945035379b71f1d676d42affecb39ac
diablo1303 <diabloposeidon@hotmail.com>
2025-03-03 17:52:25 +0800

    优化 FastJsonUtils.readJsonFile 方法
    - 优化了 readJsonFile 方法的实现，使用了 try-with-resources语句自动关闭资源
    -引入 BufferedReader 逐行读取文件内容，提高效率
    - 添加了方法的详细注释，提高代码可读性
    -调整了导入语句，使用 java.io.* 通配符导入所需类

--- geelato-utils/src/main/java/cn/geelato/utils/FastJsonUtils.java
+++ geelato-utils/src/main/java/cn/geelato/utils/FastJsonUtils.java
@@ -1,9 +1,6 @@
 package cn.geelato.utils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -11,17 +8,25 @@ import java.nio.charset.StandardCharsets;
  */
 public class FastJsonUtils {
 
+    /**
+     * 从指定路径的文件中读取JSON格式的字符串。
+     *
+     * @param filePath JSON文件的路径。
+     * @return 文件中读取到的JSON格式的字符串。
+     * @throws IOException 如果在读取文件过程中发生I/O错误。
+     */
     public static String readJsonFile(String filePath) throws IOException {
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
-        Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        int ch = 0;
-        StringBuffer sb = new StringBuffer();
-        while ((ch = reader.read()) != -1) {
-            sb.append((char) ch);
+        // 使用 try-with-resources 自动关闭资源
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+             Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            // 逐行读取文件内容
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
         }
-        reader.close();
-        inputStream.close();
-
-        return sb.toString();
     }
 }

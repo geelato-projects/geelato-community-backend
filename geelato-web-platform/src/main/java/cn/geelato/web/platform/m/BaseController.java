243670a86bc4600559b86da7c90a5114867850e3
diablo1303 <diabloposeidon@hotmail.com>
2025-04-01 12:10:35 +0800

    重构 BaseController 类
    - 移除了未使用的 List 导入
    - 为 setHttpServletRequest、setDao 和 setRuleService 方法添加了注释
    - 删除了过时的 getFilterGroup 方法
    - 优化了剩余 getFilterGroup 方法的代码结构
    - 添加了 formatMapKey 方法的注释

--- geelato-web-platform/src/main/java/cn/geelato/web/platform/m/BaseController.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/m/BaseController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -35,24 +34,44 @@ public class BaseController extends ParameterOperator implements InitializingBea
     protected RuleService ruleService;
     protected HttpServletResponse response;
 
+    /**
+     * 通过Spring框架的@Autowired注解自动注入HttpServletRequest对象
+     * 这个方法主要用于设置HttpServletRequest对象，以便在类内部使用
+     *
+     * @param httpServletRequest Servlet请求对象，用于获取请求相关的信息
+     */
     @Autowired
     protected void setHttpServletRequest(HttpServletRequest httpServletRequest) {
         this.request = httpServletRequest;
     }
 
+    /**
+     * 通过Spring框架的@Autowired注解自动注入Dao对象
+     * 这个方法主要用于设置Dao对象，以便在类内部使用
+     *
+     * @param dao 数据访问对象，用于执行数据库操作
+     */
     @Autowired
     protected void setDao(@Qualifier("primaryDao") Dao dao) {
         this.dao = dao;
     }
 
+    /**
+     * 通过Spring框架的@Autowired注解自动注入RuleService对象
+     * 这个方法主要用于设置RuleService对象，以便在类内部使用
+     *
+     * @param ruleService 规则服务对象，用于执行规则操作
+     */
     @Autowired
     protected void setRuleService(RuleService ruleService) {
         this.ruleService = ruleService;
     }
 
     /**
-     * 在每个子类方法调用之前先调用
-     * 设置request,response,session这三个对象
+     * 通过Spring框架的@ModelAttribute注解自动注入HttpServletResponse对象
+     * 这个方法主要用于设置HttpServletResponse对象，以便在类内部使用
+     *
+     * @param response Servlet响应对象，用于获取响应相关的信息
      */
     @ModelAttribute
     public void setReqAndRes(HttpServletRequest request, HttpServletResponse response) {
@@ -81,107 +100,29 @@ public class BaseController extends ParameterOperator implements InitializingBea
     }
 
     /**
-     * 根据接口传递的参数，构建查询条件
+     * 获取指定元素类型的过滤组。
+     *
+     * @param elementType 元素类型
+     * @return 指定元素类型的过滤组
+     * @throws ParseException 如果解析过程中发生错误，则抛出该异常
      */
-    public FilterGroup getFilterGroup(Class elementType, Map<String, List<String>> operatorMap) throws ParseException {
-        return this.getFilterGroup(elementType, this.request, operatorMap);
-    }
-
-    @Deprecated
-    public FilterGroup getFilterGroup(Class elementType, HttpServletRequest request, Map<String, List<String>> operatorMap) throws ParseException {
-        Map<String, Object> params = this.getQueryParameters(elementType, request, false);
-        return this.getFilterGroup(params, operatorMap);
+    public FilterGroup getFilterGroup(Class elementType) throws ParseException {
+        Map<String, Object> params = this.getQueryParameters(elementType, this.request, true);
+        return this.getFilterGroup(params);
     }
 
     /**
-     * 构建查询条件
+     * 获取指定元素类型的过滤组，并设置请求体参数。
+     *
+     * @param elementType    元素类型
+     * @param requestBodyMap 请求体参数
+     * @param isOperation    是否为操作参数
+     * @return 指定元素类型的过滤组
+     * @throws ParseException 如果解析过程中发生错误，则抛出该异常
      */
-    private FilterGroup getFilterGroup(Map<String, Object> params, Map<String, List<String>> operatorMap) throws ParseException {
-        FilterGroup filterGroup = new FilterGroup();
-        if (params != null && !params.isEmpty()) {
-            if (operatorMap != null && !operatorMap.isEmpty()) {
-                // 排除查询
-                List<String> excludes = operatorMap.get("excludes");
-                if (excludes != null && !excludes.isEmpty()) {
-                    for (String list : excludes) {
-                        if (params.get(list) != null) {
-                            params.remove(list);
-                        }
-                    }
-                }
-                // 不等于查询
-                List<String> isNulls = operatorMap.get("isNulls");
-                if (isNulls != null && !isNulls.isEmpty()) {
-                    for (String list : isNulls) {
-                        Object value = params.get(list);
-                        if (value != null && "NULL".equalsIgnoreCase(String.valueOf(value))) {
-                            filterGroup.addFilter(list, FilterGroup.Operator.nil, String.valueOf(1));
-                            params.remove(list);
-                        }
-                    }
-                }
-                // 模糊查询
-                List<String> contains = operatorMap.get("contains");
-                if (contains != null && !contains.isEmpty()) {
-                    for (String list : contains) {
-                        if (params.get(list) != null && Strings.isNotBlank(String.valueOf(params.get(list)))) {
-                            filterGroup.addFilter(list, FilterGroup.Operator.contains, String.valueOf(params.get(list)));
-                            params.remove(list);
-                        }
-                    }
-                }
-                // 存在于列表查询
-                List<String> consists = operatorMap.get("consists");
-                if (consists != null && !consists.isEmpty()) {
-                    for (String list : consists) {
-                        if (params.get(list) != null && Strings.isNotBlank(String.valueOf(params.get(list)))) {
-                            filterGroup.addFilter(list, FilterGroup.Operator.in, String.valueOf(params.get(list)));
-                            params.remove(list);
-                        }
-                    }
-                }
-                // 时间查询
-                List<String> intervals = operatorMap.get("intervals");
-                if (intervals != null && !intervals.isEmpty()) {
-                    for (String list : intervals) {
-                        Object value = params.get(list);
-                        if (value == null || Strings.isBlank(String.valueOf(value))) {
-                            continue;
-                        }
-                        String[] times = null;
-                        // 2024-08-01,2024-08-08; 2024-08-01,; ,2024-08-08
-                        if (value instanceof String) {
-                            times = String.valueOf(value).split(",");
-                            if (times.length == 2) {
-                                if (Strings.isNotBlank(times[0])) {
-                                    filterGroup.addFilter(list, FilterGroup.Operator.gte, SDF_DATE_START.format(SDF_DATE.parse(times[0])));
-                                }
-                                if (Strings.isNotBlank(times[1])) {
-                                    filterGroup.addFilter(list, FilterGroup.Operator.lte, SDF_DATE_FINISH.format(SDF_DATE.parse(times[1])));
-                                }
-                                params.remove(list);
-                            }
-                        }
-                        if (value.getClass().isArray()) {
-                            times = (String[]) value;
-                            if (times.length == 2 && Strings.isNotBlank(times[0]) && Strings.isNotBlank(times[1])) {
-                                filterGroup.addFilter(list, FilterGroup.Operator.gte, SDF_DATE_START.format(SDF_DATE.parse(times[0])));
-                                filterGroup.addFilter(list, FilterGroup.Operator.lte, SDF_DATE_FINISH.format(SDF_DATE.parse(times[1])));
-                                params.remove(list);
-                            }
-                        }
-                    }
-                }
-            }
-            // 对等查询
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                if (entry.getValue() != null && Strings.isNotBlank(entry.getValue().toString())) {
-                    filterGroup.addFilter(entry.getKey(), entry.getValue().toString());
-                }
-            }
-        }
-
-        return filterGroup;
+    public FilterGroup getFilterGroup(Class elementType, Map<String, Object> requestBodyMap, boolean isOperation) throws ParseException {
+        Map<String, Object> params = this.getQueryParameters(elementType, requestBodyMap, isOperation);
+        return this.getFilterGroup(params);
     }
 
     /**
@@ -190,24 +131,6 @@ public class BaseController extends ParameterOperator implements InitializingBea
      * @param elementType 元素类型
      * @return 指定元素类型的过滤组
      * @throws ParseException 如果解析过程中发生错误，则抛出该异常
-     */
-    public FilterGroup getFilterGroup(Class elementType) throws ParseException {
-        return this.getFilterGroup(elementType, this.request);
-    }
-
-    @Deprecated
-    public FilterGroup getFilterGroup(Class elementType, HttpServletRequest request) throws ParseException {
-        Map<String, Object> params = this.getQueryParameters(elementType, request, true);
-        return this.getFilterGroup(params);
-    }
-
-    public FilterGroup getFilterGroup(Class elementType, Map<String, Object> requestBodyMap, boolean isOperation) throws ParseException {
-        Map<String, Object> params = this.getQueryParameters(elementType, requestBodyMap, isOperation);
-        return this.getFilterGroup(params);
-    }
-
-    /**
-     * 构建查询条件
      */
     private FilterGroup getFilterGroup(Map<String, Object> params) throws ParseException {
         FilterGroup filterGroup = new FilterGroup();
@@ -264,6 +187,12 @@ public class BaseController extends ParameterOperator implements InitializingBea
         return filterGroup;
     }
 
+    /**
+     * 格式化查询参数的键。
+     *
+     * @param key 查询参数的键
+     * @return 格式化后的查询参数键
+     */
     private Map<String, String> formatMapKey(String key) {
         Map<String, String> map = new HashMap<>();
         map.put("key", key);

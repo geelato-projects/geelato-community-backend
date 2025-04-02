8bfe419ad7ea956e9dd9a560a79391bc15cd045b
diablo1303 <diabloposeidon@hotmail.com>
2025-04-02 16:53:48 +0800

    整合枚举类
    - 将多个模块的枚举类整合到 ArcoEnum 中
    - 新增了多个枚举类型，包括权限类型、RESTful配置类型、系统配置相关枚举、Excel 相关枚举等
    - 优化了枚举类的结构，按功能模块进行分类

--- geelato-web-platform/src/main/java/cn/geelato/web/platform/m/arco/enums/ArcoEnum.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/m/arco/enums/ArcoEnum.java
@@ -1,9 +1,24 @@
 package cn.geelato.web.platform.m.arco.enums;
 
+import cn.geelato.core.enums.*;
+import cn.geelato.utils.enums.LocaleEnum;
+import cn.geelato.utils.enums.TimeUnitEnum;
+import cn.geelato.web.platform.enums.PermissionTypeEnum;
+import cn.geelato.web.platform.enums.RestfulConfigType;
 import cn.geelato.web.platform.m.arco.entity.SelectOptionData;
+import cn.geelato.web.platform.m.base.enums.*;
+import cn.geelato.web.platform.m.excel.enums.*;
 import cn.geelato.web.platform.m.file.enums.AttachmentServiceEnum;
 import cn.geelato.web.platform.m.file.enums.AttachmentSourceEnum;
+import cn.geelato.web.platform.m.ocr.enums.DictDisposeEnum;
+import cn.geelato.web.platform.m.ocr.enums.MetaTypeEnum;
 import cn.geelato.web.platform.m.ocr.enums.RuleTypeEnum;
+import cn.geelato.web.platform.m.script.enums.AlternateTypeEnum;
+import cn.geelato.web.platform.m.security.enums.*;
+import cn.geelato.web.platform.m.settings.enums.MessageSendStatus;
+import cn.geelato.web.platform.m.syspackage.enums.PackageSourceEnum;
+import cn.geelato.web.platform.m.syspackage.enums.PackageStatusEnum;
+import cn.geelato.web.platform.m.zxing.enums.*;
 import lombok.Getter;
 
 import java.lang.reflect.Field;
@@ -13,9 +28,70 @@ import java.util.List;
 
 @Getter
 public enum ArcoEnum {
-    RULE_TYPE_ENUM("RuleTypeEnum", RuleTypeEnum.class),
+    // 根包枚举
+    PERMISSION_TYPE_ENUM("PermissionTypeEnum", PermissionTypeEnum.class),
+    RESTFUL_CONFIG_TYPE("RestfulConfigType", RestfulConfigType.class),
+    // 基础相关枚举
+    SYS_CONFIG_VALUE_TYPE_ENUM("SysConfigValueTypeEnum", SysConfigValueTypeEnum.class),
+    SYS_CONFIG_PURPOSE_ENUM("SysConfigPurposeEnum", SysConfigPurposeEnum.class),
+    SCHEDULE_TYPE_ENUM("ScheduleTypeEnum", ScheduleTypeEnum.class),
+    SCHEDULE_LOG_TYPE_ENUM("ScheduleLogTypeEnum", ScheduleLogTypeEnum.class),
+    SCHEDULE_LOG_STATUS_ENUM("ScheduleLogStatusEnum", ScheduleLogStatusEnum.class),
+    RESPONSE_PARAM_TYPE_ENUM("ResponseParamTypeEnum", ResponseParamTypeEnum.class),
+    MODEL_COLUMN_PCL_ENUM("ModelColumnPclEnum", ModelColumnPclEnum.class),
+    APPROVAL_STATUS_ENUM("ApprovalStatusEnum", ApprovalStatusEnum.class),
+    // Excel相关枚举
+    EXCEL_ALIGNMENT_ENUM("ExcelAlignmentEnum", ExcelAlignmentEnum.class),
+    EXCEL_MULTI_SCENE_TYPE_ENUM("ExcelMultiSceneTypeEnum", ExcelMultiSceneTypeEnum.class),
+    WORD_TABLE_LOOP_TYPE_ENUM("WordTableLoopTypeEnum", WordTableLoopTypeEnum.class),
+    EXCEL_COLUMN_TYPE_RULE_ENUM("ExcelColumnTypeRuleEnum", ExcelColumnTypeRuleEnum.class),
+    EXCEL_EVALUATION_ENUM("ExcelEvaluationEnum", ExcelEvaluationEnum.class),
+    EXCEL_COLUMN_TYPE_ENUM("ExcelColumnTypeEnum", ExcelColumnTypeEnum.class),
+    // 文件相关枚举
     ATTACHMENT_SERVICE_ENUM("AttachmentServiceEnum", AttachmentServiceEnum.class),
-    ATTACHMENT_SOURCE_ENUM("AttachmentSourceEnum", AttachmentSourceEnum.class);
+    ATTACHMENT_SOURCE_ENUM("AttachmentSourceEnum", AttachmentSourceEnum.class),
+    // OCR相关枚举
+    RULE_TYPE_ENUM("RuleTypeEnum", RuleTypeEnum.class),
+    META_TYPE_ENUM("MetaTypeEnum", MetaTypeEnum.class),
+    DICT_DISPOSE_ENUM("DictDisposeEnum", DictDisposeEnum.class),
+    // 脚本相关枚举
+    SCRIPT_RESPONSE_PARAM_TYPE_ENUM("ScriptResponseParamTypeEnum", ResponseParamTypeEnum.class),
+    ALTERNATE_TYPE_ENUM("AlternateTypeEnum", AlternateTypeEnum.class),
+    // 安全相关枚举
+    ROLE_TYPE_ENUM("RoleTypeEnum", RoleTypeEnum.class),
+    VALID_TYPE_ENUM("ValidTypeEnum", ValidTypeEnum.class),
+    ORG_TYPE_ENUM("OrgTypeEnum", OrgTypeEnum.class),
+    IS_DEFAULT_ORG_ENUM("IsDefaultOrgEnum", IsDefaultOrgEnum.class),
+    ENCODING_SERIAL_TYPE_ENUM("EncodingSerialTypeEnum", EncodingSerialTypeEnum.class),
+    ENCODING_ITEM_TYPE_ENUM("EncodingItemTypeEnum", EncodingItemTypeEnum.class),
+    AUTH_CODE_ACTION("AuthCodeAction", AuthCodeAction.class),
+    // 设置相关枚举
+    MESSAGE_SEND_STATUS("MessageSendStatus", MessageSendStatus.class),
+    // 系统包相关枚举
+    PACKAGE_STATUS_ENUM("PackageStatusEnum", PackageStatusEnum.class),
+    PACKAGE_SOURCE_ENUM("PackageSourceEnum", PackageSourceEnum.class),
+    // 条码相关枚举
+    BARCODE_TYPE_ENUM("BarcodeTypeEnum", BarcodeTypeEnum.class),
+    BARCODE_SIZE_UNIT_ENUM("BarcodeSizeUnitEnum", BarcodeSizeUnitEnum.class),
+    BARCODE_PICTURE_FORMAT_ENUM("BarcodePictureFormatEnum", BarcodePictureFormatEnum.class),
+    BARCODE_FONT_STYLE_ENUM("BarcodeFontStyleEnum", BarcodeFontStyleEnum.class),
+    BARCODE_FONT_POSITION_ENUM("BarcodeFontPositionEnum", BarcodeFontPositionEnum.class),
+    BARCODE_FONT_ALIGN_ENUM("BarcodeFontAlignEnum", BarcodeFontAlignEnum.class),
+    // Core相关枚举
+    VIEW_TYPE_ENUM("ViewTypeEnum", ViewTypeEnum.class),
+    MYSQL_DATA_TYPE_ENUM("MysqlDataTypeEnum", MysqlDataTypeEnum.class),
+    TABLE_SOURCE_TYPE_ENUM("TableSourceTypeEnum", TableSourceTypeEnum.class),
+    TABLE_TYPE_ENUM("TableTypeEnum", TableTypeEnum.class),
+    TABLE_FOREIGN_ACTION("TableForeignAction", TableForeignAction.class),
+    LINKED_ENUM("LinkedEnum", LinkedEnum.class),
+    ENABLE_STATUS_ENUM("EnableStatusEnum", EnableStatusEnum.class),
+    DIALECTS("Dialects", Dialects.class),
+    DELETE_STATUS_ENUM("DeleteStatusEnum", DeleteStatusEnum.class),
+    COLUMN_SYNCED_ENUM("ColumnSyncedEnum", ColumnSyncedEnum.class),
+    COLUMN_ENCRYPTED_ENUM("ColumnEncryptedEnum", ColumnEncryptedEnum.class),
+    // Utils相关枚举
+    TIME_UNIT_ENUM("TimeUnitEnum", TimeUnitEnum.class),
+    LOCALE_ENUM("LocaleEnum", LocaleEnum.class);
 
     private final String code;
     private final Class<?> clazz;

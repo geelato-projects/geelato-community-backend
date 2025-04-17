65ea8b243c7e46bca37258ae38385de6e309a76b
geelato <geelato@126.com>
2025-04-17 16:20:17 +0800

    优化：页面加载方法

--- geelato-web-platform/src/main/java/cn/geelato/web/platform/m/base/rest/PageController.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/m/base/rest/PageController.java
@@ -27,14 +27,50 @@ public class PageController extends BaseController {
 
     /**
      * 基于页面id或页面的扩展id（树节点id）获取页面定义及页面自定义信息
-     * 排除已删除的记录
+     *
+     * 用于运行时，页面渲染时使用。返回的页面中没有页面源码字段
      *
      * @param idType “pageId”或“extendId”
      * @param id     id值
-     * @return {id,type,appId,code,releaseContent,pageCustom}，其中pageCustom为不同用户对该页面的自定义信息
+     * @return {id,type,appId,code,releaseContent,pageCustom,pagePermission}，其中pageCustom为不同用户对该页面的自定义信息
      */
     @RequestMapping(value = {"/getPageAndCustom/{idType}/{id}", "getPageAndCustom/{idType}/{id}/*"}, method = RequestMethod.GET, produces = MediaTypes.APPLICATION_JSON_UTF_8)
     public ApiResult<HashMap<String, Object>> getPageAndCustom(@PathVariable String idType, @PathVariable String id) {
+        return getPage(idType, id, false,true,true);
+    }
+
+    /**
+     *  用于设计时，基于页面id，返回页面的完整配置信息
+     *
+     * @param pageId
+     * @return {id,type,appId,code,releaseContent,sourceContent,pageCustom,pagePermission}
+     */
+    @RequestMapping(value = {"/getPageById/{pageId}"}, method = RequestMethod.GET, produces = MediaTypes.APPLICATION_JSON_UTF_8)
+    public ApiResult<HashMap<String, Object>> getPageById(@PathVariable String pageId) {
+        return getPage("pageId", pageId,  true,false,false);
+    }
+
+    /**
+     *  用于设计时，基于页面的扩展id（树节点id），返回页面的完整配置信息
+     *
+     * @param extendId
+     * @return {id,type,appId,code,releaseContent,sourceContent,pageCustom,pagePermission}
+     */
+    @RequestMapping(value = {"/getPageByExtendId/{extendId}"}, method = RequestMethod.GET, produces = MediaTypes.APPLICATION_JSON_UTF_8)
+    public ApiResult<HashMap<String, Object>> getPageByExtendId(@PathVariable String extendId) {
+        return getPage("extendId", extendId, true,false,false);
+    }
+
+    /**
+     * 获取页面配置信息
+     * @param idType id类型，pageId或extendId
+     * @param id pageId或extendId
+     * @param withSourceContent 是否返回源代码，默认不返回
+     * @param withCustomConfig 是否返回该用户此页面的自定义配置，默认不返回
+     * @param withPermission 是否返回该用户此页面的权限信息，默认不返回
+     * @return {id,type,appId,code,releaseContent,sourceContent,pageCustom,pagePermission}
+     */
+    private ApiResult<HashMap<String, Object>> getPage(String idType,String id,Boolean withSourceContent,Boolean withCustomConfig,Boolean withPermission) {
         try {
             // 获取页面定义信息
             AppPage page = null;
@@ -69,33 +105,48 @@ public class PageController extends BaseController {
                 pageMap.put("appId", page.getAppId());
                 pageMap.put("code", page.getCode());
                 pageMap.put("releaseContent", page.getReleaseContent());
+                if(withSourceContent){
+                    pageMap.put("sourceContent", page.getSourceContent());
+                }else{
+                    pageMap.put("sourceContent", "");
+                }
 
                 User user = SessionCtx.getCurrentUser();
                 // 用户自定义信息
-                String pageCustomKey = "platform_app_page_custom_" + page.getId() + '_' + user.getUserId();
-                if (!CacheUtil.exists(pageCustomKey) || CacheUtil.get(pageCustomKey) == null) {
-                    ApiPagedResult<List<Map<String, Object>>> apiPagedResult = ruleService.queryForMapList("{\"platform_my_page_custom\":{\"@fs\":\"id,cfg,pageId\",\"creator|eq\":\"" + user.getUserId() + "\",\"pageId|eq\":\"" + page.getId() + "\",\"delStatus|eq\":0,\"@p\":\"1,1\"}}", false);
-                    if (apiPagedResult.getDataSize() > 0) {
-                        CacheUtil.put(pageCustomKey, ((List<?>) apiPagedResult.getData()).get(0));
+                if(withCustomConfig){
+                    String pageCustomKey = "platform_app_page_custom_" + page.getId() + '_' + user.getUserId();
+                    if (!CacheUtil.exists(pageCustomKey) || CacheUtil.get(pageCustomKey) == null) {
+                        ApiPagedResult<List<Map<String, Object>>> apiPagedResult = ruleService.queryForMapList("{\"platform_my_page_custom\":{\"@fs\":\"id,cfg,pageId\",\"creator|eq\":\"" + user.getUserId() + "\",\"pageId|eq\":\"" + page.getId() + "\",\"delStatus|eq\":0,\"@p\":\"1,1\"}}", false);
+                        if (apiPagedResult.getDataSize() > 0) {
+                            CacheUtil.put(pageCustomKey, ((List<?>) apiPagedResult.getData()).get(0));
+                        }
                     }
+                    pageMap.put("pageCustom", CacheUtil.get(pageCustomKey));
+                }else{
+                    pageMap.put("pageCustom", "");
                 }
-                pageMap.put("pageCustom", CacheUtil.get(pageCustomKey));
+
 
                 // 用户对该页面的操作权限
-                HashMap<String, Object> params = new HashMap<String, Object>(1);
-                params.put("userId", user.getUserId());
-                params.put("object", page.getId());
-                params.put("appId", page.getAppId());
-                params.put("type", "ep");
+                if(withPermission){
+                    HashMap<String, Object> params = new HashMap<String, Object>(1);
+                    params.put("userId", user.getUserId());
+                    params.put("object", page.getId());
+                    params.put("appId", page.getAppId());
+                    params.put("type", "ep");
 
-                String pagePermissionKey = "platform_app_page_permission_" + page.getId() + '_' + user.getUserId();
-                if (!CacheUtil.exists(pagePermissionKey) || CacheUtil.get(pagePermissionKey) == null) {
-                    List<Map<String, Object>> permsList = dao.queryForMapList("query_permission_code_and_rule_by_role_user", params);
-                    if (permsList != null && !permsList.isEmpty()) {
-                        CacheUtil.put(pagePermissionKey, permsList);
+                    String pagePermissionKey = "platform_app_page_permission_" + page.getId() + '_' + user.getUserId();
+                    if (!CacheUtil.exists(pagePermissionKey) || CacheUtil.get(pagePermissionKey) == null) {
+                        List<Map<String, Object>> permsList = dao.queryForMapList("query_permission_code_and_rule_by_role_user", params);
+                        if (permsList != null && !permsList.isEmpty()) {
+                            CacheUtil.put(pagePermissionKey, permsList);
+                        }
                     }
+                    pageMap.put("pagePerms", CacheUtil.get(pagePermissionKey));
+                }else {
+                    pageMap.put("pagePerms", "");
                 }
-                pageMap.put("pagePerms", CacheUtil.get(pagePermissionKey));
+
 
                 return ApiResult.success(pageMap);
             } else {

94c7368449ea7c781e98a30bd4de340ed935173f
gechenvegstall <maketheidsohard@gmail.com>
2025-04-22 11:19:44 +0800

    修复切换数据库的问题

--- geelato-core/src/main/java/cn/geelato/core/gql/parser/JsonTextParser.java
+++ geelato-core/src/main/java/cn/geelato/core/gql/parser/JsonTextParser.java
@@ -10,19 +10,33 @@ import java.util.Date;
 import java.util.Map;
 
 public class JsonTextParser {
+    protected final String FN_UPDATE_AT = "updateAt";
+    protected final String FN_UPDATER = "updater";
+    protected final String FN_UPDATER_NAME = "updater";
+
+    protected final String FN_CREATE_AT = "createAt";
+    protected final String FN_CREATOR = "creator";
+    protected final String FN_CREATOR_NAME = "creatorName";
+    protected final String FN_TENANT_CODE = "tenantCode";
+    protected final String FN_BU_ID = "buId";
+    protected final String FN_DEPT_ID = "deptId";
+
+    protected final String FN_DEL_STATUS = "delStatus";
+    protected final String FN_ENABLE_STATUS = "enableStatus";
+
     protected final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DateUtils.DATETIME);
     protected static final MetaManager metaManager = MetaManager.singleInstance();
 
     protected void putBaseDefaultField(SessionCtx sessionCtx, Map<String, Object> params, CommandValidator validator) {
         String newDataString = simpleDateFormat.format(new Date());
-        if (validator.hasKeyField("updateAt")) {
-            params.put("updateAt", newDataString);
+        if (validator.hasKeyField(FN_UPDATE_AT)) {
+            params.put(FN_UPDATE_AT, newDataString);
         }
-        if (validator.hasKeyField("updater")) {
-            params.put("updater", SessionCtx.getUserId());
+        if (validator.hasKeyField(FN_UPDATER)) {
+            params.put(FN_UPDATER, SessionCtx.getUserId());
         }
-        if (validator.hasKeyField("updaterName")) {
-            params.put("updaterName",  SessionCtx.getUserName());
+        if (validator.hasKeyField(FN_UPDATER_NAME)) {
+            params.put(FN_UPDATER_NAME,  SessionCtx.getUserName());
         }
     }
 }
--- geelato-web-platform/src/main/java/cn/geelato/web/platform/boot/DynamicDatasourceHolder.java
+++ geelato-web-platform/src/main/java/cn/geelato/web/platform/boot/DynamicDatasourceHolder.java
@@ -1,7 +1,7 @@
 package cn.geelato.web.platform.boot;
 
 public class DynamicDatasourceHolder {
-    public static final ThreadLocal<String> dataSourceContextHolder = new ThreadLocal<String>();
+    public static final ThreadLocal<String> dataSourceContextHolder = new ThreadLocal<>();
     public static String getDataSourceKey() {
         return dataSourceContextHolder.get();
     }

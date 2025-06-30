236eb31ffa07472f1eeef58b764e301098ac6fd6
gechenvegstall <chengx@bingosoft.net>
2025-06-30 20:59:56 +0800

    默认开启加密配置

--- geelato-core/src/main/java/cn/geelato/core/GlobalContext.java
+++ geelato-core/src/main/java/cn/geelato/core/GlobalContext.java
@@ -3,7 +3,7 @@ package cn.geelato.core;
 @SuppressWarnings("ALL")
 public class GlobalContext {
 
-    protected final static Integer __SecurityLevel__ = 0;
+    protected final static Integer __SecurityLevel__ = 2;
     protected final static Boolean __ColumnEncrypt__ = __SecurityLevel__ > 0;
     protected final static Boolean __ApiEncrypt__ = __SecurityLevel__ > 1;
     protected final static String __Environment__ = "development";

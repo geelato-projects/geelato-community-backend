56f7a1447264e5d0f0944aea8b5c1ee8e24943cc
gechenvegstall <maketheidsohard@gmail.com>
2025-07-16 14:59:13 +0800

    修改脚本改为全部清除再安装

--- maven_install.bat
+++ maven_install.bat
@@ -4,34 +4,34 @@ CHCP 65001
 
 
 cd geelato-parent
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-lang
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-utils
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-core
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-security
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-common-web
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-dynamic-datasource
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-plugin-manager
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-reactor-platform
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-web-platform
-call mvn install -Dmaven.test.skip=true
+call mvn clean install -U -Dmaven.test.skip=true
 
 cd ../geelato-web-quickstart
 call mvn clean install -U -Dmaven.test.skip=true

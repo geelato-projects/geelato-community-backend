9610f5a011d8860f6291dc183116ada211a4b1fb
geelato <geelato@126.com>
2025-08-21 13:48:21 +0800

    Update maven_install.bat

--- maven_install.bat
+++ maven_install.bat
@@ -4,36 +4,36 @@ CHCP 65001
 
 
 cd geelato-parent
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-lang
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-utils
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-core
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-security
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-web-common
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-dynamic-datasource
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-plugin-manager
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-reactor-platform
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-web-platform
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ../geelato-web-quickstart
-call mvn clean install -U -Dmaven.test.skip=true
+call mvn clean install -U -D maven.test.skip=true
 
 cd ..

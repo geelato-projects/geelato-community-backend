7bf59ae0ff7ec5832123ebaa12e1968ec29d18ee
diablo1303 <diabloposeidon@hotmail.com>
2025-09-08 15:39:42 +0800

    类属性补全注解

--- geelato-core/src/main/java/cn/geelato/core/meta/model/entity/TableForeign.java
+++ geelato-core/src/main/java/cn/geelato/core/meta/model/entity/TableForeign.java
@@ -13,60 +13,65 @@ import java.util.Map;
 @Getter
 @Setter
 @Title(title = "实体外键关系")
-@Entity(name = "platform_dev_table_foreign",catalog = "platform")
+@Entity(name = "platform_dev_table_foreign", catalog = "platform")
 public class TableForeign extends BaseSortableEntity implements EntityEnableAble {
 
     @Title(title = "应用Id")
     @Col(name = "app_id")
     private String appId;
 
+    @Title(title = "主表数据库")
     @Col(name = "main_table_schema")
     private String mainTableSchema;
 
+    @Title(title = "主表数据库表Id")
     @Col(name = "main_table_id")
     private String mainTableId;
 
-    @Col(name = "main_table")
     @Title(title = "主表表名")
+    @Col(name = "main_table")
     private String mainTable;
-    @Col(name = "main_table_col")
+
     @Title(title = "主表表名字段")
+    @Col(name = "main_table_col")
     private String mainTableCol;
 
+    @Title(title = "外键关联表数据库")
     @Col(name = "foreign_table_schema")
     private String foreignTableSchema;
 
+    @Title(title = "外键关联表数据库表Id")
     @Col(name = "foreign_table_id")
     private String foreignTableId;
 
-    @Col(name = "foreign_table")
     @Title(title = "外键关联表表名")
+    @Col(name = "foreign_table")
     private String foreignTable;
 
-    @Col(name = "foreign_table_col")
     @Title(title = "外键关联表字段")
+    @Col(name = "foreign_table_col")
     private String foreignTableCol;
 
-    @Col(name = "description")
     @Title(title = "描述")
+    @Col(name = "description")
     private String description;
 
     @Title(title = "启用状态", description = "1表示启用、0表示未启用")
     @Col(name = "enable_status")
     private int enableStatus = ColumnDefault.ENABLE_STATUS_VALUE;
 
-    @Col(name = "delete_action")
     @Title(title = "删除时触发操作")
+    @Col(name = "delete_action")
     private String deleteAction = ColumnDefault.FOREIGN_ACTION_VALUE;// 删除时
 
-    @Col(name = "update_action")
     @Title(title = "更新时触发操作")
+    @Col(name = "update_action")
     private String updateAction = ColumnDefault.FOREIGN_ACTION_VALUE;// 更新时
 
     public TableForeign() {
     }
 
-    public TableForeign(Map<String,Object> map) {
+    public TableForeign(Map<String, Object> map) {
         this.mainTable = map.get("main_table") == null ? null : map.get("main_table").toString();
         this.mainTableCol = map.get("main_table_col") == null ? null : map.get("main_table_col").toString();
         this.foreignTable = map.get("foreign_table") == null ? null : map.get("foreign_table").toString();
--- geelato-web-common/src/main/java/cn/geelato/web/common/security/Org.java
+++ geelato-web-common/src/main/java/cn/geelato/web/common/security/Org.java
@@ -12,11 +12,18 @@ import lombok.Setter;
 @Entity(name = "platform_org",catalog = "platform")
 @Title(title = "组织")
 public class Org extends BaseSortableEntity {
+    @Title(title = "组织名称")
     private String name;
+    @Title(title = "组织编码")
     private String code;
+    @Title(title = "父组织编码")
     private String pid;
+    @Title(title = "组织类型")
     private String type;
+    @Title(title = "组织分类")
     private String category;
+    @Title(title = "组织状态")
     private int status;
+    @Title(title = "组织描述")
     private String description;
 }

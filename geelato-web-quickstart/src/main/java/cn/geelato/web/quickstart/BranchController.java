6766e768eabb7ebed7d9d7022081e7df5f8f5340
gechenvegstall <maketheidsohard@gmail.com>
2026-05-21 12:17:01 +0800

    增加sentry监控

--- geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/BranchController.java
+++ geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/BranchController.java
@@ -1,13 +1,12 @@
 package cn.geelato.web.quickstart;
 
 import cn.geelato.lang.api.ApiResult;
+import io.sentry.Sentry;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import cn.geelato.web.common.annotation.ApiRestController;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -26,6 +25,20 @@ public class BranchController {
             return ApiResult.success("default");
         }
         return ApiResult.success(value);
+    }
+
+    @GetMapping("/sentry")
+    public ApiResult<String> sentry() throws IOException {
+        Resource resource = new ClassPathResource("properties/sentry.properties");
+        Properties properties = new Properties();
+        try (InputStream in = resource.getInputStream()) {
+            properties.load(in);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String name : properties.stringPropertyNames()) {
+            sb.append(name).append("=").append(properties.getProperty(name)).append("\n");
+        }
+        return ApiResult.success(sb.toString().trim());
     }
 }
 

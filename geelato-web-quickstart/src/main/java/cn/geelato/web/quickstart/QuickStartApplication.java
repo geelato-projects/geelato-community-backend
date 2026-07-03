2e41ab3bc8de8fe85a21d5db95b401d46fac641f
gechenvegstall <maketheidsohard@gmail.com>
2026-07-04 01:26:44 +0800

    修复认证缺陷

--- geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/QuickStartApplication.java
+++ geelato-web-quickstart/src/main/java/cn/geelato/web/quickstart/QuickStartApplication.java
@@ -1,6 +1,9 @@
 package cn.geelato.web.quickstart;
 
+import cn.geelato.web.platform.boot.BootApplication;
 import lombok.extern.slf4j.Slf4j;
+//import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,15 +11,21 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
+@SpringBootApplication
 @EnableConfigurationProperties
+//@MapperScan("cn.geelato.workflow.*.mapper")
 @EnableCaching
 @EnableAsync(proxyTargetClass = true)
 @Slf4j
-@SpringBootApplication(scanBasePackages = {"cn.geelato"})
-public class QuickStartApplication {
+public class QuickStartApplication extends BootApplication {
+
+    @Override
+    public void run(String... strings) throws Exception {
+        log.info("QuickStartApplication>run");
+        super.run(strings);
+    }
 
     public static void main(String[] args) {
-        log.info("Starting QuickStartApplication");
         SpringApplication.run(QuickStartApplication.class, args);
     }
 }
